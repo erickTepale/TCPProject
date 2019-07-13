3d35c1059b5b8013284e71ea5df2ea94fa78aa89
Erick Tepale <e.tepale@gmail.com>
2019-07-13 08:02:38 -0400

    getting error trying to initialize program

--- src/main/java/com/gottit/TCPProject/Service/CMService.java
+++ src/main/java/com/gottit/TCPProject/Service/CMService.java
@@ -1,20 +1,39 @@
 package com.gottit.TCPProject.Service;
 
 import com.gottit.TCPProject.Model.ChannelMessage;
+import com.gottit.TCPProject.Model.CompositeKey.ChannelMessagePK;
+import com.gottit.TCPProject.Model.Message;
 import com.gottit.TCPProject.Repository.ChannelMessageRepository;
+import com.gottit.TCPProject.Repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CMService {
     @Autowired
-    ChannelMessageRepository repository;
+    ChannelMessageRepository CMrepository;
 
-    public List<ChannelMessage> findAll(Long channel_id){
+    @Autowired
+    MessageRepository MessageRepository;
+
+    public Iterable<ChannelMessage> findAll(Long channel_id){
         //maybe for each return the messages that belong in the channel?
-        return repository.findAllByChannelMessagePK_Channel_id(channel_id);
+        return CMrepository.findAllByChannelMessagePK_ChannelID(channel_id);
+    }
+
+    //can get channel
+    public Message create(Message message, Long channel_id){
+        //store the message in table
+        Message temp = MessageRepository.save(message);
+
+        //create primary composite key for ChannelMessage
+        ChannelMessagePK pk = new ChannelMessagePK(channel_id, temp.getMessage_id());
+
+        //store row into ChannelMessage table in database
+        CMrepository.save(new ChannelMessage(pk));
+
+        //return the newly created message
+        return temp;
     }
 
 }
--- src/main/java/com/gottit/TCPProject/Service/MessageService.java
+++ src/main/java/com/gottit/TCPProject/Service/MessageService.java
@@ -3,9 +3,9 @@ package com.gottit.TCPProject.Service;
 import com.gottit.TCPProject.Model.Message;
 import com.gottit.TCPProject.Repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@Service
 public class MessageService {
     @Autowired
     private MessageRepository repository;
